a2bcf723ccff110bbd2c3ebfd6864debf36c56f9
phowson <phil@howson.net>
2017-02-04 00:21:46 +0000

    Simple basic data loading and saving tools

--- src/net/howson/phil/recommendation/testdata/DummyDataGenerator.java
+++ src/net/howson/phil/recommendation/testdata/DummyDataGenerator.java
@@ -1,10 +1,95 @@
 package net.howson.phil.recommendation.testdata;
 
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Random;
+
 public class DummyDataGenerator {
 
-	
-	public static void main(String[] args) {
-		
+	private static final int NUM_PRODUCTS = 100_000;
+	private static final int NUM_CUSTOMERS = 100_000;
+	private static final int NUM_TRANSACTIONS = 30_000_000;
+	private static final double customerCacuhyScale = 0.1;
+	private static final double productCacuhyScale = 0.1;
+
+	private static final int CORRELATED_PRODUCTS = 100;
+
+	private static final int CORRELATED_PRODUCTS_MID = CORRELATED_PRODUCTS / 2;
+	private static final int BUFFER_SIZE = 1024 * 1024;
+	private static final double CORRELATION = 0.7;
+
+	private final Random r = new Random();
+	private final PrintStream out;
+
+	public DummyDataGenerator(String[] args) throws FileNotFoundException {
+		out = new PrintStream(new BufferedOutputStream(new FileOutputStream(args[0]), BUFFER_SIZE));
 	}
-	
+
+	public void run() {
+
+		for (int i = 0; i < NUM_TRANSACTIONS; ++i) {
+			int customer;
+			do {
+				double x = nextCacuhy() * customerCacuhyScale;
+				if (x < 0) {
+					x = -x;
+				}
+				customer = (int) (x*NUM_CUSTOMERS);
+			} while (customer > NUM_CUSTOMERS || customer < 0);
+
+			int product;
+			do {
+				double x = nextCacuhy() * productCacuhyScale;
+				if (x < 0) {
+					x = -x;
+				}
+				product = (int) (x * NUM_PRODUCTS);
+			} while (product > NUM_PRODUCTS || product < 0);
+
+			out.print("customer " + customer);
+			out.print(",");
+			printProduct(product);
+			out.println();
+			
+			if (product<CORRELATED_PRODUCTS && r.nextDouble() < CORRELATION) {
+				out.print("customer " + customer);
+				out.print(",");
+				if (product<CORRELATED_PRODUCTS_MID) {
+					printProduct(product + CORRELATED_PRODUCTS_MID);
+				} else {
+					printProduct(product - CORRELATED_PRODUCTS_MID);
+				}
+				out.println();
+				
+			}
+			
+			
+		}
+		out.close();
+
+	}
+
+	private void printProduct(int product) {
+		if (product<CORRELATED_PRODUCTS) {
+			out.print("correlated product " );
+			if (product<CORRELATED_PRODUCTS_MID) {
+				out.print(product + " A");
+			} else {
+				out.print((product-CORRELATED_PRODUCTS_MID) + " B");
+			}
+		} else {
+			out.print("product" +product);
+		}
+	}
+
+	private double nextCacuhy() {
+		return r.nextGaussian() / r.nextGaussian();
+	}
+
+	public static void main(String[] args) throws Exception {
+		new DummyDataGenerator(args).run();
+	}
+
 }
--- src/net/howson/phil/recommendation/util/ConsendedDataWriter.java
+++ src/net/howson/phil/recommendation/util/ConsendedDataWriter.java
@@ -5,41 +5,63 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
-public class ConsendedDataWriter implements Closeable{
+import gnu.trove.procedure.TObjectIntProcedure;
+import gnu.trove.procedure.TObjectProcedure;
+
+public class ConsendedDataWriter implements Closeable, TObjectIntProcedure<String> {
 	private final OutputStream outputStream;
 	private final OutputStream metaOutputStream;
 	private final ByteBuffer ob = ByteBuffer.allocateDirect(8);
-	private final byte [] b = ob.array();
-	
-	
+	private final byte[] b = ob.array();
+	private final ByteBuffer ob2 = ByteBuffer.allocateDirect(1024);
+	private final byte[] b2 = ob2.array();
+
 	public ConsendedDataWriter(OutputStream outputStream, OutputStream countsOutputStream) {
 		super();
 		this.outputStream = outputStream;
 		this.metaOutputStream = countsOutputStream;
 	}
-	
+
 	public void output(int customerId, int productId) throws IOException {
 		ob.position(0);
 		ob.putInt(customerId);
-		ob.putInt(productId);		
+		ob.putInt(productId);
 		this.outputStream.write(b);
 	}
-	
-	public void outputMeta(Meta meta) {
-		
+
+	public void outputMeta(Meta meta) throws IOException {
+		ob.position(0);
+		ob.putInt(meta.getCdict().size());
+		ob.putInt(meta.getPdict().size());
+		this.metaOutputStream.write(b);
+		ob.position(0);
+		ob.putInt(meta.getTcount());
+		this.metaOutputStream.write(b, 0, 4);
+
+		meta.getCdict().forEachEntry(this);
+
 	}
-	
-	
-	
-	
-	
+
+	@Override
+	public boolean execute(String a, int b) {
+		ob2.position(0);
+		ob2.putInt(a.length());
+		ob2.put(a.getBytes());
+		ob2.putInt(b);
+
+		try {
+			metaOutputStream.write(b2, 0, ob2.position());
+		} catch (IOException e) {
+			// Ugly throw, improve
+			throw new RuntimeException(e);
+		}
+		return true;
+	}
+
 	@Override
 	public void close() throws IOException {
 		this.outputStream.close();
 		this.metaOutputStream.close();
 	}
-	
-	
-	
-	
+
 }
